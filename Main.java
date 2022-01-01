9a1c53fba7931928594a0372023335a03092265d
Wiem Medimagh <91318202+WiemMedimagh@users.noreply.github.com>
2022-01-01 17:00:13 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+package tn.isima.twetter;
+
+
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.IntWritable.Comparator;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+import org.apache.hadoop.fs.FileSystem;
+
+import org.apache.hadoop.io.Text;
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "max share");
+        job.setJarByClass(Main.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(NumMaxReducer.class);
+        job.setSortComparatorClass(Comparator.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        Path inputFilePath = new Path(args[0]);
+        Path outputFilePath = new Path(args[1]);
+        FileInputFormat.addInputPath(job, inputFilePath);
+        FileOutputFormat.setOutputPath(job, outputFilePath);
+        
+        
+        FileSystem fs = FileSystem.newInstance(conf);
+        if (fs.exists(outputFilePath)) {
+            fs.delete(outputFilePath, true);
+        }
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
