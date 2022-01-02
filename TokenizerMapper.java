ffa4d3142999365491a66bd2d788c719d6fe57ef
Wiem Medimagh <91318202+WiemMedimagh@users.noreply.github.com>
2022-01-02 10:25:40 +0100

    Update TokenizerMapper.java

--- TokenizerMapper.java
+++ TokenizerMapper.java
@@ -5,29 +5,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-
-public  class TokenizerMapper
-extends Mapper<LongWritable, Text, Text, IntWritable>{
-
-
-private IntWritable nbrlike= new IntWritable();
-
-private Text Key=new Text();
-public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
-    String line = value.toString();
-
-	String[] data=line.split("\t");
-	
-	Key.set(data[9]);
-	intnumber_of_likes= Integer.parseInt(data[1]);
-	nbrlike.set(number_of_likes);
-	
-		
-	System.out.println("author="+Key+"number_of_likes="+ nbrlike);
-	context.write(Key,nbrlike);
-		
-		
-
-}
+public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+       public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+              String[] tokens = value.toString().split(",");
+              String cle = tokens[0].substring(0,1);
+              int valeur = Integer.parseInt(tokens[1]);
+              context.write(new Text(cle), new IntWritable(valeur));
+       }
 }
