bf0a7bc34c719295fb64766b421b3f458e0e0fe7
Wiem Medimagh <91318202+WiemMedimagh@users.noreply.github.com>
2022-01-02 12:34:05 +0100

    Update and rename nbrReducer.java to NbrReducer.java

--- NbrReducer.java
+++ NbrReducer.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-public static class nbrReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+public static class NbrReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
        public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException,InterruptedException {
               int sum = 0;
               for (IntWritable val : values) {
